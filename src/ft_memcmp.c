d2a43f63e86e1f520b4686dc28176d1518af1c1a
tbolkova <tbolkova@student.42wolfsburg.de>
2023-08-26 11:58:42 +0200

    added memcmp main

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: tbolkova <tbolkova@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 18:05:13 by tbolkova          #+#    #+#             */
-/*   Updated: 2023/06/17 16:17:57 by tbolkova         ###   ########.fr       */
+/*   Updated: 2023/08/26 11:58:16 by tbolkova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,24 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		i++;
 	}
 	return (0);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str1[] = "Hello World!";
+	char str2[] = "Hello World!";
+	char str3[] = "Hello!";
+	printf("\e[0;32mft_memcmp: \n\e[0;37m");
+	printf("%d\n", ft_memcmp(str1, str2, 5));
+	printf("%d\n", ft_memcmp(str1, str2, 10));
+	printf("%d\n", ft_memcmp(str1, str3, 5));
+	printf("%d\n", ft_memcmp(str1, str3, 10));
+
+	printf("\e[0;31mmemcmp: \n\e[0;37m");
+	printf("%d\n", memcmp(str1, str2, 5));
+	printf("%d\n", memcmp(str1, str2, 10));
+	printf("%d\n", memcmp(str1, str3, 5));
+	printf("%d\n", memcmp(str1, str3, 10));
 }
